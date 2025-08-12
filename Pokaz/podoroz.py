8a7a74c437ebcf57d5f9d7338a94deab46d3a833
j269 <j269@pjwstk.edu.pl>
2025-08-12 15:33:19 +0200

    init commit

--- Pokaz/podoroz.py
+++ Pokaz/podoroz.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
 import sqlite3
 import requests
+
 app = FastAPI()
 conn = sqlite3.connect
+
+def get_db_connection():
+    conn=sqlite3.connect
+    conn.row_factory = sqlite3.Row
+    return conn
 
 @app.get("/health")
 def health():
