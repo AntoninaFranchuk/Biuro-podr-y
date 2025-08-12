76c164050438ab973f3f0f997ebebbea796e517b
j273 <j273@pjwstk.edu.pl>
2025-08-12 16:20:03 +0200

    init commit

--- Pokaz/podoroz.py
+++ Pokaz/podoroz.py
@@ -45,42 +45,6 @@ def create_healthl(destination: str, month: str, price_pln: int):
     new_health = {"id": new_id, "destination": destination, "month": month, "price_pln": price_pln}
     health.append(new_health)
     return new_health
-from fastapi import FastAPI
-from pydantic import BaseModel
-import sqlite3
-import requests
-
-database="travel.db"
-app = FastAPI()
-
-conn = sqlite3.connect('travel.db', check_same_thread=False)
-conn.row_factory = sqlite3.Row
-cur=conn.cursor()
-
-with sqlite3.connect(database) as conn:
-    cur.execute("""
-    CREATE TABLE IF NOT EXISTS travel( 
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        destination TEXT NOT NULL,
-        month TEXT NOT NULL,
-        price_pln REAL INTEGER NOT NULL,
-        UNIQUE(destination,month)
-    )
-    """)
-
-class travel(BaseModel):
-    destination: str
-    month:str
-    price_pln:int
-class traveldestinationUpdate(BaseModel):
-    destination: str
 
 
-def get_db_connection():
-    conn=sqlite3.connect
-    conn.row_factory = sqlite3.Row
-    return conn
 
-@app.get("/health")
-def health():
-    return {"status": "ok"}
